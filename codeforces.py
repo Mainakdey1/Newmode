f5888946a416530acb00caca3493f1066ceb0882
brokenwifi9 <81181897+Mainakdey1@users.noreply.github.com>
2023-09-18 23:04:30 +0530

    codeforces-api

---
+++ codeforces.py
@@ -0,0 +1,11 @@
+import urllib3
+import regex
+
+
+url= "https://codeforces.com/api/contest.list?gym=false"
+conn_pool=urllib3.PoolManager()
+resp= conn_pool.request("GET",url)
+
+ret_data=resp.data.decode("utf-8")
+print(ret_data)
+
